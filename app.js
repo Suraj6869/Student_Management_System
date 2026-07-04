712542f4330dc2dfb5f4044fc4eb18d9a9f046a2
SURAJ6869 <suraj.programs@gmail.com>
2026-07-04 20:17:42 +0530

    lESSON 2 - Added MVC project Structure

--- app.js
+++ app.js
@@ -3,9 +3,11 @@ const app = express();
 
 const PORT = 5000;
 
-app.get('/', (req,res) => {
-    res.send("Welcome User to Student Management System");
-})
+const homeRoute = require('./routes/homeRoute');
+
+app.use(express.json()); // acts as a middleware to parse incoming JSON requests
+
+app.use('/', homeRoute);
 
 app.listen(PORT, () => {
     console.log(`Server is running on http://localhost:${PORT}`);
---
+++ controllers/homeController.js
@@ -0,0 +1,7 @@
+const homePage = (req,res) => {
+    res.send("Welcome User to Student Management System");
+};
+
+module.exports = {
+    homePage
+};
