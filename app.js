0e31c4d8013726a86568f09d96c9208a9cfee9b3
SURAJ6869 <suraj.programs@gmail.com>
2026-07-04 20:05:54 +0530

    Lesson 1 - Project Initialized with Express Server

---
+++ app.js
@@ -0,0 +1,12 @@
+const express = require('express');
+const app = express();
+
+const PORT = 5000;
+
+app.get('/', (req,res) => {
+    res.send("Welcome to Student Management System");
+})
+
+app.listen(PORT, () => {
+    console.log(`Server is running on http://localhost:${PORT}`);
+});
